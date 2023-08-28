060d5b129ff82e46d3f397847c27e06bc7145da3
Shailesh Rathod <143385142+shaileshrathod1232@users.noreply.github.com>
2023-08-29 00:27:43 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+class calculator 
+{
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Basic Calculator");
+        System.out.println("Available operations: +, -, *, /");
+
+        System.out.print("Enter the first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter an operator: ");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        double num2 = sc.nextDouble();
+
+        double result = 0.0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+}
+}
